03fc204eeb7f9374cc9325a51459e743dbbceb35
Simon Virgo <simon.virgo@rwth-aachen.de>
2018-11-28 18:21:15 +1000

    new structure in gempy sandbox

--- sandbox/gempy_sandbox_brisbane.py
+++ sandbox/gempy_sandbox_brisbane.py
@@ -9,32 +9,40 @@ from sandbox.Sandbox import Calibration
 
 #TODO: make calibrations a mandatory argument in ALL Classes!!!
 
+
 class Scale:
-    def __init__(self, associated_calibration=None, xy_isometric=True, extent=None):
-        if isinstance(associated_calibration, Calibration):
-            self.associated_calibration = associated_calibration
+    def __init__(self, calibration=None, xy_isometric=True, extent=None):
+        if isinstance(calibration, Calibration):
+            self.calibration = calibration
         else:
-            print("you must pass a ")
+            raise TypeError("you must pass a valid calibration instance")
         self.xy_isometric = xy_isometric
         self.scale = [None, None, None]
         self.pixel_size = [None, None]
         self.output_res = None
 
-        if extent == None:  # extent should be array with shape (6,) or convert to list?
-            self.extent = self.model.grid.extent # try, except: put scale to pixel scale of sandbox
+        if extent is None:  # extent should be array with shape (6,) or convert to list?
+            self.extent = numpy.asarray([
+                self.calibration.calibration_data['x_lim'][0],
+                self.calibration.calibration_data['x_lim'][1],
+                self.calibration.calibration_data['y_lim'][0],
+                self.calibration.calibration_data['y_lim'][1],
+                self.calibration.calibration_data['z_range'][0],
+                self.calibration.calibration_data['x_range'][1],
+                ])
 
         else:
-            self.extent = extent  # check: array with 6 entries!
+            self.extent = numpy.asarray(extent)  # check: array with 6 entries!
 
     def calculate_scales(self):
         """
         calculates the factors for the coordinates transformation kinect-extent
         :return:
         """
-        self.output_res = (self.associated_calibration.calibration_data['x_lim'][1] -
-                           self.associated_calibration.calibration_data['x_lim'][0],
-                           self.associated_calibration.calibration_data['y_lim'][1] -
-                           self.associated_calibration.calibration_data['y_lim'][0])
+        self.output_res = (self.calibration.calibration_data['x_lim'][1] -
+                           self.calibration.calibration_data['x_lim'][0],
+                           self.calibration.calibration_data['y_lim'][1] -
+                           self.calibration.calibration_data['y_lim'][0])
         self.pixel_size[0] = float(self.extent[1] - self.extent[0]) / float(self.output_res[0])
         self.pixel_size[1] = float(self.extent[3] - self.extent[2]) / float(self.output_res[1])
 
@@ -50,16 +58,26 @@ class Scale:
         self.scale[0] = self.pixel_size[0]
         self.scale[1] = self.pixel_size[1]
         self.scale[2] = float(self.extent[5] - self.extent[4]) / (
-                self.associated_calibration.calibration_data['z_range'][1] -
-                self.associated_calibration.calibration_data['z_range'][0])
+                self.calibration.calibration_data['z_range'][1] -
+                self.calibration.calibration_data['z_range'][0])
         print("scale in Model units/ mm (X,Y,Z): " + str(self.scale))
 
     # TODO: manually define zscale and either lower or upper limit of Z, adjust rest accordingly.
 
+
 class Grid:
     def __init__(self, calibration=None, scale=None,):
-
-
+        if isinstance(calibration, Calibration):
+            self.calibration = calibration
+        else:
+            raise TypeError("you must pass a valid calibration instance")
+        if isinstance(scale, Scale):
+            self.scale= scale
+        else:
+            self.scale = Scale(calibration=self.calibration)
+            print("no scale provided or scale invalid. A default scale instance is used")
+        self.empty_depth_grid=None
+        self.create_empty_depth_grid()
 
     def create_empty_depth_grid(self):
         """
@@ -67,10 +85,10 @@ class Grid:
         :return:
         """
         grid_list = []
-        self.output_res = (self.associated_calibration.calibration_data['x_lim'][1] -
-                           self.associated_calibration.calibration_data['x_lim'][0],
-                           self.associated_calibration.calibration_data['y_lim'][1] -
-                           self.associated_calibration.calibration_data['y_lim'][0])
+        self.output_res = (self.calibration.calibration_data['x_lim'][1] -
+                           self.calibration.calibration_data['x_lim'][0],
+                           self.calibration.calibration_data['y_lim'][1] -
+                           self.calibration.calibration_data['y_lim'][0])
         for x in range(self.output_res[1]):
             for y in range(self.output_res[0]):
                 grid_list.append([y * self.pixel_size[1] + self.extent[2], x * self.pixel_size[0] + self.extent[0]])
@@ -82,95 +100,90 @@ class Grid:
 
     def update_grid(self, depth):
         depth = numpy.fliplr(depth)  ##dirty workaround to get the it running with new gempy version.
-        filtered_depth = numpy.ma.masked_outside(depth, self.associated_calibration.calibration_data['z_range'][0],
-                                                 self.associated_calibration.calibration_data['z_range'][1])
+        filtered_depth = numpy.ma.masked_outside(depth, self.calibration.calibration_data['z_range'][0],
+                                                 self.calibration.calibration_data['z_range'][1])
         scaled_depth = self.extent[5] - (
-                (filtered_depth - self.associated_calibration.calibration_data['z_range'][0]) / (
-                self.associated_calibration.calibration_data['z_range'][1] -
-                self.associated_calibration.calibration_data['z_range'][0]) * (self.extent[5] - self.extent[4]))
-        rotated_depth = scipy.ndimage.rotate(scaled_depth, self.associated_calibration.calibration_data['rot_angle'],
+                (filtered_depth - self.calibration.calibration_data['z_range'][0]) / (
+                self.calibration.calibration_data['z_range'][1] -
+                self.calibration.calibration_data['z_range'][0]) * (self.extent[5] - self.extent[4]))
+        rotated_depth = scipy.ndimage.rotate(scaled_depth, self.calibration.calibration_data['rot_angle'],
                                              reshape=False)
-        cropped_depth = rotated_depth[self.associated_calibration.calibration_data['y_lim'][0]:
-                                      self.associated_calibration.calibration_data['y_lim'][1],
-                        self.associated_calibration.calibration_data['x_lim'][0]:
-                        self.associated_calibration.calibration_data['x_lim'][1]]
+        cropped_depth = rotated_depth[self.calibration.calibration_data['y_lim'][0]:
+                                      self.calibration.calibration_data['y_lim'][1],
+                        self.calibration.calibration_data['x_lim'][0]:
+                        self.calibration.calibration_data['x_lim'][1]]
 
         flattened_depth = numpy.reshape(cropped_depth, (numpy.shape(self.empty_depth_grid)[0], 1))
         depth_grid = numpy.concatenate((self.empty_depth_grid, flattened_depth), axis=1)
 
         self.depth_grid = depth_grid
 
+class Contour:
+    def __init__(self, x,y,z, start, end,step, show=True, show_labels=False, linewidth=1.0, colors=[(0, 0, 0, 1.0)]):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.start = start
+        self.end = end
+        self.step = step
+        self.show = show
+        self.show_labels = show_labels
+        self.linewidth = linewidth
+        self.colors = colors
+        self.contours = None
+
+
+    def add_contours:
+        if self.show is True:
+            self.contours=plt.contour(self.x, self.y, sewlf.z, levels=self.sub_contours, linewidths=self.linewidth, colors=self.colors)
+            if self.show_labels is True:
+                plt.clabel(self.contours,inline=0, fontsize=15, fmt='%3.0f')
+
 
 class Plot:
-    def __init__(self ,cmap = None, norm = None, lot = None):
+    """
+    handles the plotting of a sandbox model
+    add contours by providing a list of contour objects
+    """
+    def __init__(self, rasterdata=None, calibration=None, cmap=None, norm=None, lot=None, contours=None, outfile=None):
+        if isinstance(calibration, Calibration):
+            self.calibration = calibration
+        else:
+            raise TypeError("you must pass a valid calibration instance")
+        self.rasterdata=rasterdata
         self.cmap = cmap
         self.norm = norm
         self.lot = lot
+        self.contours=contours
+        self.output_res = (
+            self.calibration.calibration_data['x_lim'][1] -
+            self.calibration.calibration_data['x_lim'][0],
+            self.calibration.calibration_data['y_lim'][1] -
+            self.calibration.calibration_data['y_lim'][0]
+                           )
+        self.block = self.rasterdata.reshape((self.output_res[1], self.output_res[0]))
+        self.h = self.calibration.calibration_data['scale_factor'] * (self.output_res[1]) / 100.0
+        self.w = self.calibration.calibration_data['scale_factor'] * (self.output_res[0]) / 100.0
 
-        self.contours = True
-        self.main_contours = numpy.arange(0, 5000, 100)
-        self.sub_contours = numpy.arange(0, 5000, 25)
+        self.fig = plt.figure(figsize=(w, h), dpi=100, frameon=False)
+        self.ax = plt.Axes(self.fig, [0., 0., 1., 1.])
+        self.ax.set_axis_off()
+        self.fig.add_axes(self.ax)
+        self.outfile = outfile
 
-        self.scalar_contours = False
-        self.scalar_main_contours = numpy.arange(0.0, 1.0, 0.1)
-        self.scalar_sub_contours = numpy.arange(0.0, 1.0, 0.02)
+    def render_frame(self):
+        self.ax.pcolormesh(self.block, cmap=self.cmap, norm=self.norm)
 
-        self.show_faults = True
-        self.fault_contours = numpy.arange(0.5, 50.5, 1.0)
+        for contour in self.contours:
+            contour.add_contours()
 
-    def set_cmap(self, cmap=None, norm=None):
-        if cmap is None:
-            # plotter = gempy.PlotData2D(self.model._geo_data)
-            self.cmap = gempy.plot.cmap  # plotter._cmap
-        if norm is None:
-            self.norm = gempy.plot.norm  # plotter._norm
-            self.lot = gempy.plot.color_lot  # plotter._color_lot
 
-    def render_frame(self, depth, outfile=None):
-        self.update_grid(depth)
-
-        sol = gempy.compute_model_at(self.depth_grid, self.model)
-        scalar_field = sol.scalar_field_lith.reshape((self.output_res[1], self.output_res[0]))
-        block = sol.lith_block.reshape((self.output_res[1], self.output_res[0]))
-        h = self.associated_calibration.calibration_data['scale_factor'] * (self.output_res[1]) / 100.0
-        w = self.associated_calibration.calibration_data['scale_factor'] * (self.output_res[0]) / 100.0
-
-        fig = plt.figure(figsize=(w, h), dpi=100, frameon=False)
-        ax = plt.Axes(fig, [0., 0., 1., 1.])
-        ax.set_axis_off()
-        fig.add_axes(ax)
-        ax.pcolormesh(block, cmap=self.cmap, norm=self.norm)
-
-        if self.show_faults is True:
-            plt.contour(sol.fault_blocks[0].reshape((self.output_res[1], self.output_res[0])),
-                        levels=self.fault_contours, linewidths=3.0, colors=[(1.0, 1.0, 1.0, 1.0)])
-
-        if self.contours is True:
-            x = range(self.output_res[0])
-            y = range(self.output_res[1])
-            z = self.depth_grid.reshape((self.output_res[1], self.output_res[0], 3))[:, :, 2]
-            sub_contours = plt.contour(x, y, z, levels=self.sub_contours, linewidths=0.5, colors=[(0, 0, 0, 0.8)])
-            main_contours = plt.contour(x, y, z, levels=self.main_contours, linewidths=1.0, colors=[(0, 0, 0, 1.0)])
-            # plt.contour(lith_block[1].reshape((self.output_res[1], self.output_res[0])) levels=main_levels, linewidths=1.0, colors=[(0, 0, 0, 1.0)])
-            plt.clabel(main_contours, inline=0, fontsize=15, fmt='%3.0f')
-
-        if self.scalar_contours is True:
-            x = range(self.output_res[0])
-            y = range(self.output_res[1])
-            z = scalar_field
-            sub_contours = plt.contour(x, y, z, levels=self.scalar_sub_contours, linewidths=0.5,
-                                       colors=[(0, 0, 0, 0.8)])
-            main_contours = plt.contour(x, y, z, levels=self.scalar_main_contours, linewidths=1.0,
-                                        colors=[(0, 0, 0, 1.0)])
-            # plt.contour(lith_block[1].reshape((self.output_res[1], self.output_res[0])) levels=main_levels, linewidths=1.0, colors=[(0, 0, 0, 1.0)])
-            plt.clabel(main_contours, inline=0, fontsize=15, fmt='%3.0f')
-
-        if outfile == None:
+        if self.outfile is None:
             plt.show()
             plt.close()
         else:
-            plt.savefig(outfile, pad_inches=0)
-            plt.close(fig)
+            plt.savefig(self.outfile, pad_inches=0)
+            plt.close(self.fig)
 
 
     def create_legend(self):
@@ -178,12 +191,15 @@ class Plot:
         pass
 
 
+
+### Still to refactor:
+
 #TODO: use Descriptors
 class Model:
     _ids = count(0)
     _instances = []
 
-    def __init__(self, model, associated_calibration=None, lock=None):
+    def __init__(self, model, calibration=None, lock=None):
         self.id = next(self._ids)
         self.__class__._instances.append(weakref.proxy(self))
 
@@ -196,20 +212,20 @@ class Model:
         self.stop_threat = False
         self.lock = lock
 
-        if associated_calibration is None:
+        if calibration is None:
             try:
-                self.associated_calibration = Calibration._instances[-1]
-                print("no calibration specified, using last calibration instance created: ",self.associated_calibration)
+                self.calibration = Calibration._instances[-1]
+                print("no calibration specified, using last calibration instance created: ",self.calibration)
             except:
                 print("ERROR: no calibration instance found. please create a calibration")
                 # parameters from the model:
         else:
-            self.associated_calibration = associated_calibration
+            self.calibration = calibration
 
 
     def setup(self, start_stream=False):
         if start_stream == True:
-            self.associated_calibration.associated_projector.start_stream()
+            self.calibration.associated_projector.start_stream()
         self.calculate_scales()
         self.create_empty_depth_grid()
 
@@ -221,7 +237,7 @@ class Model:
 def run_model(model, calibration=None, kinect=None, projector=None, filter_depth=True, n_frames=5,
               sigma_gauss=4):  # continous run functions with exit handling
     if calibration == None:
-        calibration = model.associated_calibration
+        calibration = model.calibration
     if kinect == None:
         kinect = calibration.associated_kinect
     if projector == None:
