c39d6deaa55b4cbaa3931f1bb29da34fe107168e
Marco van Veen <marco.vanveen@rwth-aachen.de>
2019-09-23 16:52:59 +0200

    Tidy up classes and imports, specific imports put into classes for now

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,64 +1,23 @@
-# class infrastructre
-from abc import ABCMeta, abstractmethod
-from warnings import warn
+# TODO: Docstring!!!
 
-try:
-    import freenect
-
-    warn('Two kernels cannot access the kinect at the same time. This will lead to a sudden death of the kernel. ' \
-         'Be sure no other kernel is running before initialize a kinect object.', RuntimeWarning)
-except ImportError:
-    warn(
-        'Freenect is not installed. if you are using the Kinect Version 2 on a windows machine, use the KinectV2 class!')
-
-try:
-    from pykinect2 import PyKinectV2  # try to import Wrapper for KinectV2 Windows SDK
-    from pykinect2 import PyKinectRuntime
-
-except ImportError:
-    pass
-
-try:
-    import cv2
-    from cv2 import aruco
-
-except ImportError:
-    # warn('opencv is not installed. Object detection will not work')
-    pass
-
-import numpy
-import scipy
-import scipy.ndimage
-
-# for new projector
-import panel as pn
-from time import sleep
-
+from abc import ABCMeta
+from abc import abstractmethod
+import json
 import logging
-
-# for DummySensor
-from scipy.spatial.distance import cdist
-from scipy.interpolate import griddata
-
-#for resizing of block data:
-import skimage
+import threading
+from time import sleep
+from warnings import warn
 
 import matplotlib.pyplot as plt
 import matplotlib
-# import gempy.hackathon as hackathon
-import threading
-
-import json
+import numpy
 import pandas as pd
-
-# TODO: When we move GeoMapModule import gempy just there
-try:
-    import gempy as gp
-
-except ImportError:
-    warn('gempy not found, GeoMap Module will not work')
-
-### General settings and options ###
+import panel as pn
+import scipy
+from scipy.interpolate import griddata  # for DummySensor
+import scipy.ndimage
+from scipy.spatial.distance import cdist  # for DummySensor
+import skimage  # for resizing of block data
 
 # logging and exception handling
 verbose = False
@@ -68,6 +27,7 @@ if verbose:
                          level=logging.WARNING,
                          format= '%(asctime)s - %(levelname)s - %(message)s',
                        )
+
 
 class Sandbox:
     # Wrapping API-class
@@ -158,6 +118,12 @@ class KinectV1(Sensor):
     color_height = 480
     # TODO: Check!
 
+    def __init__(self):
+        super().__init__(*args, **kwargs)
+        import freenect
+        warn('Two kernels cannot access the Kinect at the same time. This will lead to a sudden death of the kernel. '
+             'Be sure no other kernel is running before you initialize a KinectV1 object.')
+
     def setup(self):
         print("looking for kinect...")
         ctx = freenect.init()
@@ -227,6 +193,12 @@ class KinectV2(Sensor):
     depth_height = 424
     color_width = 1920
     color_height = 1080
+
+    def __init__(self):
+        super().__init__(*args,**kwargs)
+        # Wrapper for KinectV2 Windows SDK
+        from pykinect2 import PyKinectV2
+        from pykinect2 import PyKinectRuntime
 
     def setup(self):
         self.device = PyKinectRuntime.PyKinectRuntime(
@@ -1960,224 +1932,233 @@ class BlockModule(Module):
 
 class GemPyModule(Module):
     # child class of Model
+    # TODO: When we move GeoMapModule import gempy just there
+    #try:
+    #    import gempy as gp
+    #except ImportError:
+    #    warn('gempy not found, GeoMap Module will not work')
     pass
 
 
-class GeoMapModule:
-    """
-
-    """
-
-    # TODO: When we move GeoMapModule import gempy just there
-
-    def __init__(self, geo_model, grid: Grid, geol_map: Plot):
-        """
-
-        Args:
-            geo_model:
-            grid:
-            geol_map:
-            work_directory:
-
-        Returns:
-            None
-
-        """
-
-        self.geo_model = geo_model
-        self.kinect_grid = grid
-        self.geol_map = geol_map
-
-        self.fault_line = self.create_fault_line(0, self.geo_model.geo_data_res.n_faults + 0.5001)
-        self.main_contours = self.create_main_contours(self.kinect_grid.scale.extent[4],
-                                                       self.kinect_grid.scale.extent[5])
-        self.sub_contours = self.create_sub_contours(self.kinect_grid.scale.extent[4],
-                                                     self.kinect_grid.scale.extent[5])
-
-        self.x_grid = range(self.kinect_grid.scale.output_res[0])
-        self.y_grid = range(self.kinect_grid.scale.output_res[1])
-
-        self.plot_topography = True
-        self.plot_faults = True
-
-    def compute_model(self, kinect_array):
-        """
-
-        Args:
-            kinect_array:
-
-        Returns:
-
-        """
-        self.kinect_grid.update_grid(kinect_array)
-        sol = gp.compute_model_at(self.kinect_grid.depth_grid, self.geo_model)
-        lith_block = sol[0][0]
-        fault_blocks = sol[1][0::2]
-        block = lith_block.reshape((self.kinect_grid.scale.output_res[1],
-                                    self.kinect_grid.scale.output_res[0]))
-
-        return block, fault_blocks
-
-    # TODO: Miguel: outfile folder should follow by default whatever is set in projection!
-    # TODO: Temporal fix. Eventually we need a container class or metaclass with this data
-    def render_geo_map(self, block, fault_blocks):
-        """
-
-        Args:
-            block:
-            fault_blocks:
-            outfile:
-
-        Returns:
-
-        """
-
-        self.geol_map.render_frame(block)
-
-        elevation = self.kinect_grid.depth_grid.reshape((self.kinect_grid.scale.output_res[1],
-                                                         self.kinect_grid.scale.output_res[0], 3))[:, :, 2]
-        # This line is for GemPy 1.2: fault_data = sol.fault_blocks.reshape((scalgeol_map.outfilee.output_res[1],
-        # scale.output_res[0]))
-
-        if self.plot_faults is True:
-            for fault in fault_blocks:
-                fault = fault.reshape((self.kinect_grid.scale.output_res[1], self.kinect_grid.scale.output_res[0]))
-                self.geol_map.add_contours(self.fault_line, [self.x_grid, self.y_grid, fault])
-        if self.plot_topography is True:
-            self.geol_map.add_contours(self.main_contours, [self.x_grid, self.y_grid, elevation])
-            self.geol_map.add_contours(self.sub_contours, [self.x_grid, self.y_grid, elevation])
-
-        return self.geol_map.figure
-
-
-    def create_fault_line(self,
-                          start=0.5,
-                          end=50.5,  # TODO Miguel:increase?
-                          step=1.0,
-                          linewidth=3.0,
-                          colors=[(1.0, 1.0, 1.0, 1.0)]):
-        """
-
-        Args:
-            start:
-            end:
-            step:
-            linewidth:
-            colors:
-
-        Returns:
-
-        """
-
-        self.fault_line = Contour(start=start, end=end, step=step, linewidth=linewidth,
-                                  colors=colors)
-
-        return self.fault_line
-
-    def create_main_contours(self, start, end, step=100, linewidth=1.0,
-                             colors=[(0.0, 0.0, 0.0, 1.0)], show_labels=True):
-        """
-
-        Args:
-            start:
-            end:
-            step:
-            linewidth:
-            colors:
-            show_labels:
-
-        Returns:
-
-        """
-
-        self.main_contours = Contour(start=start,
-                                     end=end,
-                                     step=step,
-                                     show_labels=show_labels,
-                                     linewidth=linewidth, colors=colors)
-        return self.main_contours
-
-    def create_sub_contours(self,
-                            start,
-                            end,
-                            step=25,
-                            linewidth=0.8,
-                            colors=[(0.0, 0.0, 0.0, 0.8)],
-                            show_labels=False
-                            ):
-        """
-
-        Args:
-            start:
-            end:
-            step:
-            linewidth:
-            colors:
-            show_labels:
-
-        Returns:
-
-        """
-
-        self.sub_contours = Contour(start=start, end=end, step=step, linewidth=linewidth, colors=colors,
-                                    show_labels=show_labels)
-        return self.sub_contours
-
-    def export_topographic_map(self, output="topographic_map.pdf"):
-        """
-
-        Args:
-            output:
-
-        Returns:
-
-        """
-        self.geol_map.create_empty_frame()
-        elevation = self.kinect_grid.depth_grid.reshape((self.kinect_grid.scale.output_res[1],
-                                                         self.kinect_grid.scale.output_res[0], 3))[:, :, 2]
-        self.geol_map.add_contours(self.main_contours, [self.x_grid, self.y_grid, elevation])
-        self.geol_map.add_contours(self.sub_contours, [self.x_grid, self.y_grid, elevation])
-        self.geol_map.save(outfile=output)
-
-    def export_geological_map(self, kinect_array, output="geological_map.pdf"):
-        """
-
-        Args:
-            kinect_array:
-            output:
-
-        Returns:
-
-        """
-
-        print("there is still a bug in the map that causes the uppermost lithology to be displayed in the basement"
-              " color. Unfortunately we do not have a quick fix for this currently... Sorry! Please fix the map "
-              "yourself, for example using illustrator")
-
-        lith_block, fault_blocks = self.compute_model(kinect_array)
-
-        # This line is for GemPy 1.2: lith_block = sol.lith_block.reshape((scale.output_res[1], scale.output_res[0]))
-
-        self.geol_map.create_empty_frame()
-
-        lith_levels = self.geo_model.potential_at_interfaces[-1].sort()
-        self.geol_map.add_lith_contours(lith_block, levels=lith_levels)
-
-        elevation = self.kinect_grid.depth_grid.reshape((self.kinect_grid.scale.output_res[1],
-                                                         self.kinect_grid.scale.output_res[0], 3))[:, :, 2]
-        # This line is for GemPy 1.2: fault_data = sol.fault_blocks.reshape((scalgeol_map.outfilee.output_res[1],
-        # scale.output_res[0]))
-
-        if self.plot_faults is True:
-            for fault in fault_blocks:
-                fault = fault.reshape((self.kinect_grid.scale.output_res[1], self.kinect_grid.scale.output_res[0]))
-                self.geol_map.add_contours(self.fault_line, [self.x_grid, self.y_grid, fault])
-
-        if self.plot_topography is True:
-            self.geol_map.add_contours(self.main_contours, [self.x_grid, self.y_grid, elevation])
-            self.geol_map.add_contours(self.sub_contours, [self.x_grid, self.y_grid, elevation])
-
-        self.geol_map.save(outfile=output)
+# class GeoMapModule:
+#     """
+#
+#     """
+#
+#     # TODO: When we move GeoMapModule import gempy just there
+#     try:
+#         import gempy as gp
+#     except ImportError:
+#         warn('gempy not found, GeoMap Module will not work')
+#
+#     def __init__(self, geo_model, grid: Grid, geol_map: Plot):
+#         """
+#
+#         Args:
+#             geo_model:
+#             grid:
+#             geol_map:
+#             work_directory:
+#
+#         Returns:
+#             None
+#
+#         """
+#
+#         self.geo_model = geo_model
+#         self.kinect_grid = grid
+#         self.geol_map = geol_map
+#
+#         self.fault_line = self.create_fault_line(0, self.geo_model.geo_data_res.n_faults + 0.5001)
+#         self.main_contours = self.create_main_contours(self.kinect_grid.scale.extent[4],
+#                                                        self.kinect_grid.scale.extent[5])
+#         self.sub_contours = self.create_sub_contours(self.kinect_grid.scale.extent[4],
+#                                                      self.kinect_grid.scale.extent[5])
+#
+#         self.x_grid = range(self.kinect_grid.scale.output_res[0])
+#         self.y_grid = range(self.kinect_grid.scale.output_res[1])
+#
+#         self.plot_topography = True
+#         self.plot_faults = True
+#
+#     def compute_model(self, kinect_array):
+#         """
+#
+#         Args:
+#             kinect_array:
+#
+#         Returns:
+#
+#         """
+#         self.kinect_grid.update_grid(kinect_array)
+#         sol = gp.compute_model_at(self.kinect_grid.depth_grid, self.geo_model)
+#         lith_block = sol[0][0]
+#         fault_blocks = sol[1][0::2]
+#         block = lith_block.reshape((self.kinect_grid.scale.output_res[1],
+#                                     self.kinect_grid.scale.output_res[0]))
+#
+#         return block, fault_blocks
+#
+#     # TODO: Miguel: outfile folder should follow by default whatever is set in projection!
+#     # TODO: Temporal fix. Eventually we need a container class or metaclass with this data
+#     def render_geo_map(self, block, fault_blocks):
+#         """
+#
+#         Args:
+#             block:
+#             fault_blocks:
+#             outfile:
+#
+#         Returns:
+#
+#         """
+#
+#         self.geol_map.render_frame(block)
+#
+#         elevation = self.kinect_grid.depth_grid.reshape((self.kinect_grid.scale.output_res[1],
+#                                                          self.kinect_grid.scale.output_res[0], 3))[:, :, 2]
+#         # This line is for GemPy 1.2: fault_data = sol.fault_blocks.reshape((scalgeol_map.outfilee.output_res[1],
+#         # scale.output_res[0]))
+#
+#         if self.plot_faults is True:
+#             for fault in fault_blocks:
+#                 fault = fault.reshape((self.kinect_grid.scale.output_res[1], self.kinect_grid.scale.output_res[0]))
+#                 self.geol_map.add_contours(self.fault_line, [self.x_grid, self.y_grid, fault])
+#         if self.plot_topography is True:
+#             self.geol_map.add_contours(self.main_contours, [self.x_grid, self.y_grid, elevation])
+#             self.geol_map.add_contours(self.sub_contours, [self.x_grid, self.y_grid, elevation])
+#
+#         return self.geol_map.figure
+#
+#
+#     def create_fault_line(self,
+#                           start=0.5,
+#                           end=50.5,  # TODO Miguel:increase?
+#                           step=1.0,
+#                           linewidth=3.0,
+#                           colors=[(1.0, 1.0, 1.0, 1.0)]):
+#         """
+#
+#         Args:
+#             start:
+#             end:
+#             step:
+#             linewidth:
+#             colors:
+#
+#         Returns:
+#
+#         """
+#
+#         self.fault_line = Contour(start=start, end=end, step=step, linewidth=linewidth,
+#                                   colors=colors)
+#
+#         return self.fault_line
+#
+#     def create_main_contours(self, start, end, step=100, linewidth=1.0,
+#                              colors=[(0.0, 0.0, 0.0, 1.0)], show_labels=True):
+#         """
+#
+#         Args:
+#             start:
+#             end:
+#             step:
+#             linewidth:
+#             colors:
+#             show_labels:
+#
+#         Returns:
+#
+#         """
+#
+#         self.main_contours = Contour(start=start,
+#                                      end=end,
+#                                      step=step,
+#                                      show_labels=show_labels,
+#                                      linewidth=linewidth, colors=colors)
+#         return self.main_contours
+#
+#     def create_sub_contours(self,
+#                             start,
+#                             end,
+#                             step=25,
+#                             linewidth=0.8,
+#                             colors=[(0.0, 0.0, 0.0, 0.8)],
+#                             show_labels=False
+#                             ):
+#         """
+#
+#         Args:
+#             start:
+#             end:
+#             step:
+#             linewidth:
+#             colors:
+#             show_labels:
+#
+#         Returns:
+#
+#         """
+#
+#         self.sub_contours = Contour(start=start, end=end, step=step, linewidth=linewidth, colors=colors,
+#                                     show_labels=show_labels)
+#         return self.sub_contours
+#
+#     def export_topographic_map(self, output="topographic_map.pdf"):
+#         """
+#
+#         Args:
+#             output:
+#
+#         Returns:
+#
+#         """
+#         self.geol_map.create_empty_frame()
+#         elevation = self.kinect_grid.depth_grid.reshape((self.kinect_grid.scale.output_res[1],
+#                                                          self.kinect_grid.scale.output_res[0], 3))[:, :, 2]
+#         self.geol_map.add_contours(self.main_contours, [self.x_grid, self.y_grid, elevation])
+#         self.geol_map.add_contours(self.sub_contours, [self.x_grid, self.y_grid, elevation])
+#         self.geol_map.save(outfile=output)
+#
+#     def export_geological_map(self, kinect_array, output="geological_map.pdf"):
+#         """
+#
+#         Args:
+#             kinect_array:
+#             output:
+#
+#         Returns:
+#
+#         """
+#
+#         print("there is still a bug in the map that causes the uppermost lithology to be displayed in the basement"
+#               " color. Unfortunately we do not have a quick fix for this currently... Sorry! Please fix the map "
+#               "yourself, for example using illustrator")
+#
+#         lith_block, fault_blocks = self.compute_model(kinect_array)
+#
+#         # This line is for GemPy 1.2: lith_block = sol.lith_block.reshape((scale.output_res[1], scale.output_res[0]))
+#
+#         self.geol_map.create_empty_frame()
+#
+#         lith_levels = self.geo_model.potential_at_interfaces[-1].sort()
+#         self.geol_map.add_lith_contours(lith_block, levels=lith_levels)
+#
+#         elevation = self.kinect_grid.depth_grid.reshape((self.kinect_grid.scale.output_res[1],
+#                                                          self.kinect_grid.scale.output_res[0], 3))[:, :, 2]
+#         # This line is for GemPy 1.2: fault_data = sol.fault_blocks.reshape((scalgeol_map.outfilee.output_res[1],
+#         # scale.output_res[0]))
+#
+#         if self.plot_faults is True:
+#             for fault in fault_blocks:
+#                 fault = fault.reshape((self.kinect_grid.scale.output_res[1], self.kinect_grid.scale.output_res[0]))
+#                 self.geol_map.add_contours(self.fault_line, [self.x_grid, self.y_grid, fault])
+#
+#         if self.plot_topography is True:
+#             self.geol_map.add_contours(self.main_contours, [self.x_grid, self.y_grid, elevation])
+#             self.geol_map.add_contours(self.sub_contours, [self.x_grid, self.y_grid, elevation])
+#
+#         self.geol_map.save(outfile=output)
 
 
 class ArucoMarkers:
@@ -2186,6 +2167,13 @@ class ArucoMarkers:
     An Area of interest can be specified, markers outside this area will be ignored
     TODO: run as loop in a thread, probably implement in API
     """
+
+    try:
+        import cv2
+        from cv2 import aruco
+    except ImportError:
+        # warn('opencv is not installed. Object detection will not work')
+        pass
 
     def __init__(self, aruco_dict=None, Area=None):
         if not aruco_dict:
