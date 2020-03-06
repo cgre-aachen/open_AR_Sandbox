a52755c3e549353eadc29d83fea665cc48e16d6b
Daniel Escallon <daniel.escallon@rwth-aachen.de>
2020-03-06 17:30:39 +0100

    [Notebook_to_SB]
    First steps for notebook-sandbox connectivity.

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1621,6 +1621,8 @@ class TopoModule(Module):
     def __init__(self, *args, **kwargs):
         # call parents' class init, use greyscale colormap as standard and extreme color labeling
         self.height = 2000
+        self.fig = None
+        self.ax = None
         super().__init__(*args, contours=True,
                          cmap='gist_earth',
                          over='k',
@@ -1628,8 +1630,8 @@ class TopoModule(Module):
                          vmin=0,
                          vmax=500,
                          contours_label=True,
+                         minor_contours=True,
                          **kwargs)
-
 
     def setup(self):
         self.norm = True
@@ -1661,7 +1663,7 @@ class TopoModule(Module):
 
         self.plot.render_frame(frame)
 
-        # if aruco Module is specified:search, update, plot aruco markers
+       # if aruco Module is specified:search, update, plot aruco markers
         if isinstance(self.Aruco, ArucoMarkers):
             self.Aruco.search_aruco()
             self.Aruco.update_marker_dict()
@@ -3052,6 +3054,73 @@ class LoadSaveTopoModule(Module):
         """
         saves a vector graphic of the contour map to disk
         """
+        pass
+
+
+class NotebookConnection(Module):
+    """
+    Class for the connectivity between Notebook plotting and sandbox image in live thread
+    """
+    def __init__(self, *args, **kwargs):
+        # call parents' class init, use greyscale colormap as standard and extreme color labeling
+        self.height = 2000
+        super().__init__(*args, contours=True,
+                         cmap='gist_earth',
+                         over='k',
+                         under='k',
+                         vmin=0,
+                         vmax=500,
+                         contours_label=True,
+                         **kwargs)
+
+    def setup(self):
+        frame = self.sensor.get_filtered_frame()
+        if self.crop:
+            self.norm = True
+            self.plot.minor_contours = True
+            frame = self.crop_frame(frame)
+            frame = self.clip_frame(frame)
+            frame = self.calib.s_max - frame
+        if self.norm:
+            frame = frame * (self.height / frame.max())
+            self.plot.vmin = 0
+            self.plot.vmax = self.height
+
+        self.plot.render_frame(frame)
+        self.projector.frame.object = self.plot.figure
+
+    def update(self):
+        # with self.lock:
+        frame = self.sensor.get_filtered_frame()
+        if self.crop:
+            frame = self.crop_frame(frame)
+            frame = self.clip_frame(frame)
+            frame = self.calib.s_max - frame
+        if self.norm:
+            frame = frame * (self.height / frame.max())
+            self.plot.vmin = 0
+            self.plot.vmax = self.height
+
+        self.plot.render_frame(frame)
+
+        #TODO: connectivity
+
+        self.fig, self.ax = self.plot_notebook()
+
+        #self.projector.frame.object =
+
+
+
+        # if aruco Module is specified:search, update, plot aruco markers
+        if isinstance(self.Aruco, ArucoMarkers):
+            self.Aruco.search_aruco()
+            self.Aruco.update_marker_dict()
+            self.Aruco.transform_to_box_coordinates()
+            self.plot.plot_aruco(self.Aruco.aruco_markers)
+
+        self.projector.trigger()  # triggers the update of the bokeh plot
+
+    def plot_notebook(self, fig, ax):
         pass
 
 
