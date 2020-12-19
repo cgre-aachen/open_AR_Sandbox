b0cb5e86f976f2f674afc4aea1eb38238800cad5
Florian Wellmann <flohorovicic@gmail.com>
2020-12-19 15:07:51 +0100

    Added test for contour adding - but still a bit hacky, not entirely sure how to implement tests for figures...

--- tests/test_modules/test_topography.py
+++ tests/test_modules/test_topography.py
@@ -3,6 +3,7 @@ from sandbox.modules import TopoModule
 import matplotlib.pyplot as plt
 import pytest
 import numpy as np
+
 file = np.load(test_data['topo'] + "DEM1.npz")
 frame = file['arr_0']
 extent = np.asarray([0, frame.shape[1], 0, frame.shape[0], frame.min(), frame.max()])
@@ -22,7 +23,8 @@ pytest.sb_params = {'frame': frame,
 def test_init():
     module = TopoModule()
     print(module)
-    
+
+
 def test_update():
     pytest.sb_params['ax'].cla()
     module = TopoModule(extent=extent)
@@ -33,16 +35,18 @@ def test_update():
               cmap=sb_params.get('cmap'), norm=sb_params.get('norm'), origin='lower')
     fig.show()
 
-def test_update_no_see():
+
+def test_update_no_sea():
     pytest.sb_params['ax'].cla()
     module = TopoModule(extent=extent)
-    module.see = False
+    module.sea = False
     sb_params = module.update(pytest.sb_params)
     ax = sb_params['ax']
     fig = sb_params['fig']
     ax.imshow(sb_params.get('frame'), vmin=sb_params.get('extent')[-2], vmax=sb_params.get('extent')[-1],
               cmap=sb_params.get('cmap'), norm=sb_params.get('norm'), origin='lower')
     fig.show()
+
 
 def test_update_no_normalized():
     pytest.sb_params['ax'].cla()
@@ -55,10 +59,20 @@ def test_update_no_normalized():
               cmap=sb_params.get('cmap'), norm=sb_params.get('norm'), origin='lower')
     fig.show()
 
+
 def test_widgets():
     module = TopoModule(extent=extent)
     widgets = module.show_widgets()
     widgets.show()
 
 
-
+def test_add_countour():
+    pytest.sb_params['ax'].cla()
+    module = TopoModule(extent=extent)
+    module.sea_contour = True
+    sb_params = module.update(pytest.sb_params)
+    ax = sb_params['ax']
+    fig = sb_params['fig']
+    ax.imshow(sb_params.get('frame'), vmin=sb_params.get('extent')[-2], vmax=sb_params.get('extent')[-1],
+              cmap=sb_params.get('cmap'), norm=sb_params.get('norm'), origin='lower')
+    fig.show()
